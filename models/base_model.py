9c66f239ec9f37af9c3778b26079dcfe88f3e16d
Magma <raymondamoateng@gmail.com>
2022-09-01 12:48:38 +0100

    Adding models

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,51 @@
-#!/usr/bin/env python3
-# this is the base model for the airbnb project
-""" this model defines all
-common attributes for other classes
-"""
-
-import uuid
+#!/usr/bin/python3
+#this is the basemodel for the AirBnB project
+""" Class BaseModel """
 from datetime import datetime
+from uuid import uuid4
+import models
 
 
 class BaseModel:
-    """this class class defines all
-    common attr for all other classes
-    """
+    """ construct """
 
-    def __init__(self):
-        """the class constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """ Construct """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        """the unofficial repr of this class"""
-        string = "[{}] ({}) {}".format(self.__class__.__name__,
-                                       self.id, self.__dict__)
-        return string
+        """ String """
+        return('[' + type(self).__name__ + '] (' + str(self.id) +
+               ') ' + str(self.__dict__))
 
     def save(self):
-        """updates 'updated_at' with the currenct time"""
+        """ save function """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """ returns a dictionary containing
-        all keys/values of __dict__ of the instance
-        """
-        self.__dict__['__class__'] = self.__class__.__name__
-        self.__dict__['created_at'] = self.created_at.isoformat()
-        self.__dict__['updated_at'] = self.updated_at.isoformat()
-        return self.__dict__
+        """ Return a dictonary """
+        aux_dict = self.__dict__.copy()
+        aux_dict['__class__'] = self.__class__.__name__
+        aux_dict['created_at'] = self.created_at.isoformat()
+        aux_dict['updated_at'] = self.updated_at.isoformat()
+        return aux_dict
